0181e966dcb27c6ebc9d49be0da101b7bab3f879
Jared Maddox <jared@adadevelopersacademy.org>
2019-11-05 10:59:55 -0800

    full index action with tests

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,14 +1,14 @@
 class PetsController < ApplicationController
   def index
-    # @pets = Pet.all
-    render json: { ready_for_lunch: "yassss" }, status: :ok
+    pets = Pet.all
+    render json: pets, status: :ok
   end
 
 
 
-  private
+  # private
 
-    def pet_params
-      params.require(:pet).permit(:name, :age, :human)
-    end
+  #   def pet_params
+  #     params.require(:pet).permit(:name, :age, :human)
+  #   end
 end
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -8,5 +8,34 @@ describe PetsController do
       expect(response.header['Content-Type']).must_include 'json'
       must_respond_with :ok
     end
+
+    it "responds with the expected list of pets" do
+      get pets_path
+
+      names = Pet.columns.map do |c| 
+        c.name
+      end
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_instance_of Array
+      expect(body.size).must_equal Pet.count
+      body.each do |pet_hash| 
+        expect(pet_hash).must_be_instance_of Hash
+        expect(pet_hash.keys.sort).must_equal names.sort
+      end
+    end
+
+    it "will respond with an empty array when there are no pets" do
+      # Arrange
+      Pet.destroy_all
+  
+      # Act
+      get pets_path
+      body = JSON.parse(response.body)
+  
+      # Assert
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+    end
   end
 end
