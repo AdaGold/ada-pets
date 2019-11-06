9d22bc5b2fa6f0ce8fa55fcba064395d575d2579
beccaelenzil <beccaelenzil@gmail.com>
2019-11-06 11:34:12 -0800

    create action and tests

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,8 +1,8 @@
-KEYS = [:id, :name, :age, :human]
+KEYS = ["id", "name", "age", "human"].sort
 
 class PetsController < ApplicationController
   def index
-    pets = Pet.all.as_json(only: KEYS)
+    pets = Pet.all.as_json(only: PET_KEYS)
     render json: pets, status: :ok
   end
 
@@ -10,16 +10,25 @@ class PetsController < ApplicationController
     pet_id = params[:id]
     pet = Pet.find_by(id: pet_id)
     if pet
-      render json: pet.as_json(only: KEYS) #status: :ok
+      render json: pet.as_json(only: PET_KEYS) #status: :ok
       return
     else
-      render json: pet, status: :not_found
-      #render json: {"errors"=>["not found"]}, status: :not_found
+      #render json: pet, status: :not_found
+      render json: {"errors"=>["not found"]}, status: :not_found
       return
     end
   end
 
+  def create
+    pet = Pet.new(pet_params)
 
+    if pet.save
+      render json: pet.as_json(only: [:id]), status: :created
+      return
+    else
+      render json: {ok: false, errors: pet.errors.messages}, status: :bad_request
+    end
+  end
 
   private
 
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,28 +1,17 @@
 require 'test_helper'
+PET_KEYS = ["id", "name", "age", "human"]
 
 describe PetsController do
   describe "index" do
-    it "responds with JSON and success" do
+
+    it "responds with JSON, success, and an array of pet hashes" do
       get pets_path
       
-      # Assert
-      expect(response.header['Content-Type']).must_include 'json'
-      must_respond_with :success
-      
-    end
+      body = check_response(expected_type: Array) 
     
-    it "responds with an array of pet hashes" do
-      get pets_path
-
-      # Get the body of the response
-      body = JSON.parse(response.body)
-
-      
-      # Assert
-      expect(body).must_be_instance_of Array
       body.each do |pet|
         expect(pet).must_be_instance_of Hash
-        expect(pet.keys.sort).must_equal ["age", "human", "id", "name"]
+        expect(pet.keys.sort).must_equal PET_KEYS.sort
       end
     end
 
@@ -62,6 +51,44 @@ describe PetsController do
       expect(body).must_be_instance_of Hash
       expect(body["errors"]).must_equal ["not found"]
       must_respond_with :not_found
+    end
+
+  end
+
+  describe "create" do
+    let(:pet_data) {
+      {
+        pet: {
+          age: 13,
+          name: 'Stinker',
+          human: 'Grace'
+        }
+      }
+    }
+    it "can create a new pet" do
+      expect {
+        post pets_path, params: pet_data
+      }.must_differ 'Pet.count', 1
+
+      must_respond_with :created
+    end
+
+    it "will respond with bad_request for invalid data" do
+      # Arrange - using let from above
+      pet_data[:pet][:age] = nil
+
+      expect {
+        # Act
+        post pets_path, params: pet_data
+
+      # Assert
+      }.wont_change "Pet.count"
+
+      must_respond_with :bad_request
+
+      expect(response.header['Content-Type']).must_include 'json'
+      body = JSON.parse(response.body)
+      expect(body["errors"].keys).must_include "age"
     end
 
   end
