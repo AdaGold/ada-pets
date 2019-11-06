03e77fb1b38ca4b1ee5a6a579b32b42b2e4989bb
Jared Maddox <jared@adadevelopersacademy.org>
2019-11-06 08:58:16 -0800

    Add show action and tests

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,7 +4,17 @@ class PetsController < ApplicationController
     render json: pets, status: :ok
   end
 
+  def show
+    pet = Pet.find_by(id: params[:id])
 
+    if pet
+      render json: pet.as_json(only: [:id, :name, :age, :human])
+      return
+    else
+      render json: { ok: false, errors: ["Not Found"] }, status: :not_found
+      return
+    end
+  end
 
   # private
 
--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,5 +1,7 @@
 require 'test_helper'
 
+PET_FIELDS = ["age", "human", "id", "name"]
+
 describe PetsController do
   describe "index" do
     it "responds with JSON and success" do
@@ -36,6 +38,35 @@ describe PetsController do
       # Assert
       expect(body).must_be_instance_of Array
       expect(body).must_equal []
+    end
+  end
+
+  describe "show" do
+    it "retrieves one pet" do
+      # Arrange
+      pet = Pet.first
+
+      # Act
+      get pet_path(pet)
+      body = JSON.parse(response.body)
+
+      # Assert
+      must_respond_with :success
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body).must_be_instance_of Hash
+      expect(body.keys.sort).must_equal PET_FIELDS
+    end
+
+    it "sends back not found if the pet does not exist" do
+      # Act
+      get pet_path(-1)
+      body = JSON.parse(response.body)
+
+      # Assert
+      must_respond_with :not_found
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body).must_be_instance_of Hash
+      expect(body.keys).must_include "errors"
     end
   end
 end
